2f7352bcaf1271a974405cc99dd9bf70fa5b35d0
Alex <qwert.rock666@gmail.com>
2022-06-18 22:19:44 +0200

    ViseVerse закінчено

--- ViceVersa/urls.py
+++ ViceVersa/urls.py
@@ -20,5 +20,5 @@ from . import views
 urlpatterns = [
 
     path('', views.home),
-    path('reverse/', views.reverse)
+    path('reverse/', views.reverse, name='reverse')
 ]
--- ViceVersa/views.py
+++ ViceVersa/views.py
@@ -4,5 +4,9 @@ def home(request):
 	return render(request, 'home.html')
 
 def reverse(request):
-	return render(request, 'reverse.html', name='reverse')
+	user_text = request.GET['usertext']
+	reversed_text = user_text[::-1]
+	word_text = user_text.split()
+	number = len(word_text)
+	return render(request, 'reverse.html', {'usertext':user_text, 'reversetext':reversed_text, 'number':number})
 
